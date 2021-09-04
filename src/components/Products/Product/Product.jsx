ee2f84779aece1fc342df64eccfb67d2538958e3
chrisbrody <chrisbrody@codifyacademy.com>
2021-09-04 12:10:00 -0400

    created product display using commerce.js as backend

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -4,27 +4,27 @@ import { AddShoppingCart } from '@material-ui/icons';
 
 import useStyles from './styles';
 
-const Product = ({ product, onAddToCart }) => {
+const Product = ({ product,onAddToCart }) => {
   const classes = useStyles();
 
-  const handleAddToCart = () => onAddToCart(product.id, 1);
+  console.log(product);
 
   return (
     <Card className={classes.root}>
-        <CardMedia className={classes.media} image={product.image} title={product.name} />
+        <CardMedia className={classes.media} image={product.image.url} title={product.name} />
         <CardContent>
             <div className={classes.cardContent}>
                 <Typography variant="h5" gutterBottom>
                     {product.name}
                 </Typography>
                 <Typography variant="h5">
-                    {product.price}
+                    {product.price.formatted_with_symbol}
                 </Typography>
             </div>
-            <Typography variant="body2" color="textSecondary">{product.description}</Typography>
+            <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" />
         </CardContent>
         <CardActions disabledSpacing className={classes.cardActions}>
-            <IconButton aria-label="Add to Cart">
+            <IconButton aria-label="Add to Cart" onClick={() => onAddToCart(product.id, 1)}>
                 <AddShoppingCart />
             </IconButton>
         </CardActions>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,19 +2,18 @@ import React from "react";
 import { Grid } from '@material-ui/core';
 
 import Product from "./Product/Product";
+import useStyles from './styles';
 
-const products = [
-    {id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/6362ebb8-ad24-428c-9b2b-293e99410f42/go-flyease-shoes-V7n8cS.png' },
-    {id: 2, name: 'Macbook', description: 'Apple Macbook.', price: '$10', image: 'https://www.thunderbolttechnology.net/sites/default/files/MBP15RD-TB-2016-PT-Open105-Svr-SCREEN.png' },
-];
-
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
+    const classes = useStyles();
+    
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map( (product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product}/>
+                        <Product product={product} onAddToCart={onAddToCart} />
                     </Grid>
                 ))}
             </Grid>
